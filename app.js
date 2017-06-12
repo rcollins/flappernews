e59a82f24c451dcdf1877bd94011e7eb0870b731
Robert C Collins <Robert.C.Collins@kp.org>
2017-06-12 16:44:40 -0700

    Add Angular routing

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 var app = angular.module('flapperNews', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+
+      $urlRouterProvider.otherwise('home');
+}])
 .factory('posts', [function(){
   var o = {
     posts: [{title: 'hello', link:'', upvotes:0}]
