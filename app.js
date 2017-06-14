a9607d0f17575c6c7fe5446657e149685eda2976
rcollins <robert.collinsjr@gmail.com>
2017-06-13 23:48:33 -0700

    added routes and mongodb

--- app.js
+++ app.js
@@ -9,16 +9,22 @@ var app = angular.module('flapperNews', ['ui.router'])
           url: '/home',
           templateUrl: '/home.html',
           controller: 'MainCtrl'
+        })
+
+        .state('posts', {
+          url:'/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
         });
 
       $urlRouterProvider.otherwise('home');
 }])
 .factory('posts', [function(){
   var o = {
-    posts: [{title: 'hello', link:'', upvotes:0}]
+    posts: []
   };
   return o;
-}])
+}]);
 
 app.controller('MainCtrl', ['$scope','posts', function($scope, posts){
   $scope.test = 'Hello world!';
@@ -30,12 +36,36 @@ app.controller('MainCtrl', ['$scope','posts', function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0});
+      upvotes: 0,
+      comments:[
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Berry', body: 'Great idea but everything is wrong', upvotes: 0}
+      ]
+      });
     $scope.title = '';
     $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
-  }
+  };
+}]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if ($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0,
+        });
+
+      $scope.body = '';
+    }
 }]);
